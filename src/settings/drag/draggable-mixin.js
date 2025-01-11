870532fc998e8edbda40fa60602f1889199753cd
lcvriend <43742775+lcvriend@users.noreply.github.com>
2025-01-11 17:02:16 +0100

    Add touch screen support

--- src/settings/drag/draggable-mixin.js
+++ src/settings/drag/draggable-mixin.js
@@ -13,6 +13,7 @@ export const DraggableMixin = (superClass) => class extends superClass {
         if (!handle) return
 
         handle.addEventListener("mousedown", this.handleDragStart)
+        handle.addEventListener("touchstart", this.handleDragStart)
         return handle
     }
 
@@ -22,9 +23,12 @@ export const DraggableMixin = (superClass) => class extends superClass {
         const handle = this.shadowRoot.querySelector("drag-handle")
         if (handle) {
             handle.removeEventListener("mousedown", this.handleDragStart)
+            handle.removeEventListener("touchstart", this.handleDragStart)
         }
         document.removeEventListener("mousemove", this.handleDrag)
         document.removeEventListener("mouseup", this.handleDragEnd)
+        document.removeEventListener("touchmove", this.handleDrag, { passive: false })
+        document.removeEventListener("touchend", this.handleDragEnd)
     }
 
     get handle() {
@@ -32,25 +36,35 @@ export const DraggableMixin = (superClass) => class extends superClass {
     }
 
     handleDragStart(event) {
+        const point = event.touches ? event.touches[0] : event
         const rect = this.getBoundingClientRect()
         this.dragOffset = {
-            x: event.clientX - rect.left,
-            y: event.clientY - rect.top
+            x: point.clientX - rect.left,
+            y: point.clientY - rect.top
         }
 
-        document.addEventListener("mousemove", this.handleDrag)
-        document.addEventListener("mouseup", this.handleDragEnd)
+        if (event.touches) {
+            document.addEventListener("touchmove", this.handleDrag, { passive: false })
+            document.addEventListener("touchend", this.handleDragEnd)
+        } else {
+            document.addEventListener("mousemove", this.handleDrag)
+            document.addEventListener("mouseup", this.handleDragEnd)
+        }
         event.target.setAttribute("data-dragging", "")
     }
 
     handleDrag(event) {
-        this.style.left = `${event.clientX - this.dragOffset.x}px`
-        this.style.top = `${event.clientY - this.dragOffset.y}px`
+        const point = event.touches ? event.touches[0] : event
+        this.style.left = `${point.clientX - this.dragOffset.x}px`
+        this.style.top = `${point.clientY - this.dragOffset.y}px`
+        if (event.touches) event.preventDefault()
     }
 
     handleDragEnd() {
         document.removeEventListener("mousemove", this.handleDrag)
         document.removeEventListener("mouseup", this.handleDragEnd)
+        document.removeEventListener("touchmove", this.handleDrag)
+        document.removeEventListener("touchend", this.handleDragEnd)
 
         if (this.handle) {
             this.handle.removeAttribute("data-dragging")
